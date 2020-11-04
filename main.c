4a5150030b25920906b0a0a37a5c9b90c9cb4c6c
Yajat Vishwak <yajat472001@gmail.com>
2020-11-04 17:40:19 +0530

    hello world

---
+++ main.c
@@ -0,0 +1,191 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "queues.c"
+
+int main()
+{
+    srand(time(0));
+    int opd_counter = 0;
+    int emergency_counter = 0;
+
+    patient_t *opd_queue = malloc(sizeof(patient_t));
+    opd_queue->id = rand() % 50;
+    patient_t *emergency_queue = malloc(sizeof(patient_t));
+    emergency_queue->id = rand() % 50;
+
+    int interval = 0;
+    printf("This is a ER simulation of an hospital. It consists of a 2 queues, opd(out patient department) and emergency queues. \nIn an real world scenario, emergencies and opds patients should be balanced. \nThis simulation shows how a computer program can help in such a condition.");
+
+    char inp = getchar();
+
+    int r = rand() % 50;
+
+    for (int i = 0; i < (rand() % 10) + 1; i++)
+    {
+
+        pushPatient(emergency_queue, r);
+        r = rand() % 50;
+    }
+
+    for (int i = 0; i < (rand() % 10) + 1; i++)
+    {
+        pushPatient(opd_queue, r);
+        r = rand() % 50;
+    }
+
+    printf("Assuming Queues before the simulation starts \n");
+    printLists(emergency_queue, opd_queue);
+    printf("\n \n");
+    r = rand() % 50;
+
+    while ((inp == '\n' || inp == 'q' || inp == 'd') && interval <= SIMTIME)
+    {
+        printf("Time : %d \n", interval + 1);
+        if (inp == 'q')
+        {
+            break;
+        }
+
+        else
+        {
+
+            if (emergency_queue != NULL && opd_queue == NULL)
+            {
+                int emergency_patient = popPatient(&emergency_queue->next);
+                printf("Doctor attending emergency. ID: %d \n", emergency_patient);
+                emergency_counter++;
+                printList(emergency_queue);
+                if (count(emergency_queue) < 10)
+                {
+
+                    for (int i = 0; i < rand() % 3; i++)
+                    {
+                        r = rand() % 50;
+                        while (checkPatient(emergency_queue, r) == 1)
+                        {
+                            r = rand() % 50;
+                        }
+                        pushPatient(emergency_queue, r);
+                    }
+                }
+                if (count(opd_queue) < 10)
+                {
+
+                    for (int i = 0; i < rand() % 3; i++)
+                    {
+                        r = rand() % 50;
+                        while (checkPatient(opd_queue, r) == 1)
+                        {
+                            r = rand() % 50;
+                        }
+                        pushPatient(opd_queue, r);
+                    }
+                }
+            }
+
+            if (opd_queue != NULL && emergency_queue == NULL)
+            {
+                int opd_patient = popPatient(&opd_queue->next);
+                printf("Doctor attending OPD. ID: %i \n", opd_patient);
+                opd_counter++;
+                printList(opd_queue);
+                if (count(emergency_queue) < 10)
+                {
+
+                    for (int i = 0; i < rand() % 3; i++)
+                    {
+                        r = rand() % 50;
+                        while (checkPatient(emergency_queue, r) == 1)
+                        {
+                            r = rand() % 50;
+                        }
+                        pushPatient(emergency_queue, r);
+                    }
+                }
+                if (count(opd_queue) < 10)
+                {
+
+                    for (int i = 0; i < rand() % 3; i++)
+                    {
+                        r = rand() % 50;
+                        while (checkPatient(opd_queue, r) == 1)
+                        {
+                            r = rand() % 50;
+                        }
+                        pushPatient(opd_queue, r);
+                    }
+                }
+            }
+
+            if (emergency_queue != NULL && opd_queue != NULL)
+            {
+                float score_emergency = (float)0.5 * count(emergency_queue);
+                float score_opd = (float)0.5 * count(opd_queue);
+                int selection = 0;
+                if (score_emergency < score_opd)
+                {
+                    selection = 1;
+                }
+                if (selection == 0)
+                {
+                    int emergency_patient = popPatient(&emergency_queue->next);
+                    printf("Doctor attending emergency. ID: %d \n", emergency_patient);
+                    emergency_counter++;
+                }
+                else if (selection == 1)
+                {
+                    int opd_patient = popPatient(&opd_queue->next);
+                    printf("Patient attending emergency. ID: %i \n", opd_patient);
+                    opd_counter++;
+                }
+                if (count(emergency_queue) < 10)
+                {
+                    for (int i = 0; i < rand() % 3; i++)
+                    {
+                        r = rand() % 50;
+                        while (checkPatient(emergency_queue, r) == 1)
+                        {
+                            r = rand() % 50;
+                        }
+                        pushPatient(emergency_queue, r);
+                    }
+                }
+                if (count(opd_queue) < 10)
+                {
+
+                    for (int i = 0; i < rand() % 3; i++)
+                    {
+                        r = rand() % 50;
+                        while (checkPatient(opd_queue, r) == 1)
+                        {
+                            r = rand() % 50;
+                        }
+                        pushPatient(opd_queue, r);
+                    }
+                }
+            }
+            printf("Report \n");
+            printf("Number of patients in emergency : %d \n", count(emergency_queue));
+            printf("Number of patients in opd : %d \n", count(opd_queue));
+            printLists(emergency_queue, opd_queue);
+            inp = getchar();
+            interval++;
+        }
+    }
+    printf("\n \n");
+    if (interval - 1 == SIMTIME)
+    {
+        printf("The simulation has reached maximum allocated time of the simulation.\n This can be changed in the Queues.c file.\nNow, halting. \n");
+    }
+    else
+    {
+        printf("The simulation has been interrupted by user. Now, halting.");
+    }
+    printf("Report \n");
+    printLists(emergency_queue, opd_queue);
+    printf("Number of patients in emergency : %d \n", count(emergency_queue));
+    printf("Number of patients in opd : %d \n", count(opd_queue));
+    printf("Total Time simulation ran for : %d \n ", interval - 1);
+}
---
+++ queues.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define SIMTIME 10
+
+typedef struct patient
+{
+    int id;
+    struct patient *next;
+} patient_t;
+
+// -- Queues implemented List Defination --
+int count(patient_t *head)
+{
+    int i = 0;
+    patient_t *present = head->next;
+    while (present->next != NULL)
+    {
+        i++;
+        present = present->next;
+    }
+    return i;
+}
+void printList(patient_t *head)
+{
+
+    patient_t *present = head->next;
+    while (present->next != NULL)
+    {
+        printf("Plane id : %d \n", present->id);
+        present = present->next;
+    }
+}
+
+void pushPatient(patient_t *head, int value)
+{
+
+    patient_t *current = head;
+
+    while (current->next != NULL)
+    {
+
+        current = current->next;
+    }
+
+    current->next = malloc(sizeof(patient_t));
+    current->next->id = value;
+    current->next->next = NULL;
+}
+
+int popPatient(patient_t **head)
+{
+
+    patient_t *nextPatient = NULL;
+    int currentID = -1;
+
+    if (*head == NULL)
+    {
+        return -1;
+    }
+
+    nextPatient = (*head)->next;
+    currentID = (*head)->id;
+    free(*head);
+    *head = nextPatient;
+
+    return currentID;
+}
+
+void printLists(patient_t *list1, patient_t *list2)
+{
+    int count1 = count(list1);
+    int count2 = count(list2);
+    int max = count1 > count2 ? count1 : count2;
+    patient_t *present1 = list1->next;
+    patient_t *present2 = list2->next;
+    printf("Emergency \t| OPD \n");
+
+    for (int i = 0; i < max; i++)
+    {
+        if (present1->next == NULL)
+        {
+            printf("- \t \t| %d \n", present2->id);
+            present2 = present2->next;
+        }
+        else if (present2->next == NULL)
+        {
+            printf("%d \t \t| - \n", present1->id);
+            present1 = present1->next;
+        }
+        else
+        {
+            printf("%d \t \t| %d \n", present1->id, present2->id);
+            present1 = present1->next;
+            present2 = present2->next;
+        }
+    }
+}
+int checkPatient(patient_t *head, int id)
+{
+    patient_t *current = head;
+    while (current->next != NULL)
+    {
+        if (current->id == id)
+        {
+            return 1;
+        }
+        current = current->next;
+    }
+    return 0;
+}
+// -- End of queues defination --
